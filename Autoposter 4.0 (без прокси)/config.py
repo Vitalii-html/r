39a4067792b3c357bc3417b51acee0d69ead9494
sfjvksop <vitaliimg2023@gmail.com>
2025-06-04 20:36:15 +0300

    Add files via upload

---
+++ Autoposter 4.0 (без прокси)/config.py
@@ -0,0 +1,10 @@
+# #BOT:
+TOKEN = "7958503613:AAH56dpGjqJ3uC6ynzgyRhL9Efokoij4YlQ" # ТОКЕН БОТА
+ADMIN = 7696950686 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+USERNAME = '' # НЕ ОБЯЗАТЕЛЬНО
+
+DIR = "" # Путь к файлу, если нужно
+
+#USER: # https://my.telegram.org/auth
+API_ID = 123
+API_HASH = "1"
